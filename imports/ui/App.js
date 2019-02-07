9bd2b176e86cd44ffa8b934b9fe0604d11292b4c
Steve Choi <stevechoi93@gmail.com>
2019-02-06 16:33:23 -0800

    react rendering in .../

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,29 +1,16 @@
 import React, { Component } from "react";
-
-import Task from "./Task.js";
-
+import Layout from "./pages/Layout";
 // App component - represents the whole app
 export default class App extends Component {
-  getTasks() {
-    return [
-      { _id: 1, text: "This is task 1" },
-      { _id: 2, text: "This is task 2" },
-      { _id: 3, text: "This is task 3" }
-    ];
-  }
-
-  renderTasks() {
-    return this.getTasks().map(task => <Task key={task._id} task={task} />);
-  }
-
   render() {
     return (
       <div className="container">
         <header>
           <h1>Todo List</h1>
         </header>
+        <Layout />
 
-        <ul>{this.renderTasks()}</ul>
+        <ul />
       </div>
     );
   }
